e864d38ca105b257b739fddc425c48721e5300d6
Weverton <wevertonmartinsdigital@gmail.com>
2026-06-15 11:25:55 -0300

    Reformular o método principal para incluir o sistema de gerenciamento de veículos.
    Implementei um sistema de gerenciamento de veículos com opções para adicionar, listar e excluir veículos. Adicionei tratamento de erros para entradas inválidas.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		ArrayList<Vehicle> vehicle = new ArrayList<>();
+
+		while (true) {
+			System.out.println("-".repeat(10) + " Sistema de Gestão de Veículos " + "-".repeat(10));
+
+			System.out.println("\nOpções Disponíveis");
+			System.out.println("1- Cadastrar Veículo");
+			System.out.println("2- Listar Todos Veículos");
+			System.out.println("3- Excluir Veículo");
+
+			try {
+			    System.out.print("Opção: ");
+			    int optionMenu = scanner.nextInt();
+			    
+			} catch (InputMismatchException e) {
+			    System.out.println("Favor preencher apenas com as opções em tela.");
+			    scanner.next();
+			    continue;
+			}
+
+			break;
+		}
+		
+		scanner.close();
+	}
 }

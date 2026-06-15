74e1ccfffb563e6a77a56ea5306732985152a2c5
Weverton <wevertonmartinsdigital@gmail.com>
2026-06-15 11:26:36 -0300

    Crie uma classe `Vehicle` com atributos e construtor.

---
+++ src/Vehicle.java
@@ -0,0 +1,15 @@
+class Vehicle {
+    String vehicleBrand;
+    String vehicleModel;
+    int vehicleYear;
+    int vehicleRenavam;
+    String vehiclePlate;
+    
+    public Vehicle(String vehicleBrand, String vehicleModel, int vehicleYear, int vehicleRenavam, String vehiclePlate) {
+        this.vehicleBrand = vehicleBrand;
+        this.vehicleModel = vehicleModel;
+        this.vehicleYear = vehicleYear;
+        this.vehicleRenavam = vehicleRenavam;
+        this.vehiclePlate = vehiclePlate;
+    }
+}

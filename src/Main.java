78d00fa4925606ecf3acfa7bbce2e3accf61b0ec
Weverton <wevertonmartinsdigital@gmail.com>
2026-06-17 12:46:17 -0300

    Refatorar o método principal e os prompts de interação do usuário.
    Refactor main method for improved readability and structure.

--- src/Main.java
+++ src/Main.java
@@ -3,37 +3,51 @@ import java.util.ArrayList;
 import java.util.InputMismatchException;
 
 public class Main {
-    // Método Princípal do sistema 
-    public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		ArrayList<Vehicle> vehicle = new ArrayList<>();
+    // Método principal para execução do programa
+    public static void main (String[] args) {
+        // Objeto Scanner para entrada de dados
+        Scanner scanner = new Scanner(System.in);
 
+        // Lista de veículos cadastrados no sistema
+        ArrayList<Vehicle> vehicle = new ArrayList<>();
+
+        // Interface de interação do usuário
         boolean codeRunning = true;
+
         while (codeRunning) {
             System.out.println("=".repeat(10) + " Gestão de Veículos " + "=".repeat(10));
 
-            System.out.println("\nOpções Disponíveis");
-			System.out.println("1- Cadastrar Veículo");
-			System.out.println("2- Listar Todos Veículos");
-			System.out.println("3- Excluir Veículo");
-            System.out.println("4- Sair do Sistema");
+            System.out.println("\n" + """
+            Opções Disponíveis
+            1- Cadastrar Veículos
+            2- Listar Todos Veículos
+            3- Excluir Veículo
+            4- Sair do Sistema
+            """);
 
-            // Verificação da entrada de dados por parte do usuário
-            int userOption;
+            // Entrada e validação dos dados passado pelo usuário
+            int menuOption;
 
             try {
-                System.out.println("Opção informada: ");
-                userOption = scanner.nextInt();
+                System.out.print("Opção Selecionada: ");
+                menuOption = scanner.nextInt();
 
-                return;
+                // Direcionamento do método conforme a entrada de dados
+                switch (menuOption) {
+                    case 1:
+                        System.out.println("Deu certo!");
+                        scanner.nextLine();
+                    
+                    case 2:
+                        System.out.println("Deu certo duas vezes!");
+                        scanner.nextLine();
+                }
                 
             } catch(InputMismatchException e) {
-                System.out.println("\nOpção inválida, tente novamente!");
-                scanner.next();9
+                System.out.println("A opção precisa ser apenas números inteiros.\n");
+                scanner.nextLine();
                 continue;
             }
         }
-        
-        scanner.close(); // Finalizar a classe Scanner
     }
 }

0d6a13e722da9bb3395a58e8bbc03d230c83cc71
Weverton <wevertonmartinsdigital@gmail.com>
2026-06-22 10:35:03 -0300

    Reformular os métodos de registro e listagem de veículos

--- src/VehicleService.java
+++ src/VehicleService.java
@@ -2,60 +2,54 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class VehicleService {
-    // Método para cadastro de informações do veículo
+    // Método dedicado a inclusão de novos veículos ao programa
     public static Vehicles vehicleRegistration(Scanner scanner) {
-        System.out.println("\n" + "=".repeat(10) + " Cadastrar Veículo " + "=".repeat(10));
+        System.out.println("\n" + "=".repeat(10) + " CADASTRO DE VEÍCULOS NOVOS " + "=".repeat(10));
 
-        // Exibição das informações necessárias para cadastro de veículos
-        System.out.print("Marca: ");
-        String vehicleBrand = scanner.nextLine();
+        // Loop para validação das informações do veículo
+        boolean registeredVehicle = false;
 
-        System.out.print("Modelo: ");
-        String vehicleModel = scanner.nextLine();
+        while (!registeredVehicle) {
+            System.out.print("Marca: ");
+            String vehicleBrand = scanner.nextLine();
 
-        System.out.print("Ano: ");
-        int vehicleYear = scanner.nextInt();
+            System.out.print("Modelo: ");
+            String vehicleModel = scanner.nextLine();
 
-        System.out.print("Renavam: ");
-        int vehicleRenavam = scanner.nextInt();
-        scanner.nextLine(); // limpa o buffer
+            System.out.print("Ano: ");
+            int vehicleYear = scanner.nextInt();
 
-        System.out.print("Placa: ");
-        String vehiclePlate = scanner.nextLine();
+            System.out.print("Placa: ");
+            String vehiclePlate = scanner.nextLine();
 
-        // Objeto com os dados informados pelo usuário
+            registeredVehicle = true; // Finalizar o loop para retorna e guarda informações
+        }
+
+        // Objeto destinado a guarda os dados informados pelo usuário
         Vehicles vehicle = new Vehicles(
             vehicleBrand,
             vehicleModel,
             vehicleYear,
-            vehicleRenavam,
             vehiclePlate
         );
 
+        // Exibir a marca + nome do veículo cadastrado e retorna ao menu interativo
         System.out.println(vehicleBrand + " " + vehicleModel + " Cadastrado!\n");
         return vehicle;
     }
 
 
-    // Método para listar todos os veículos cadastrados
+    // Método dedicado a listagem de todos veículos cadastrados no programa
     public static void listVehicles(ArrayList<Vehicles> vehicles) {
-        System.out.println("=".repeat(10) + " Lista de Veículos " + "=".repeat(10));
+        System.out.println("=".repeat(10) + " LISTA DOS VEÍCULOS CADASTRADOS " + "=".repeat(10));
 
         if (vehicles.isEmpty()) {
-            System.out.println("Nenhum veículo cadastrado.\n");
+            System.out.println("Nenhum veículo cadastrado ainda.\n");
             return;
         }
 
         for (Vehicles vehicle : vehicles) {
             System.out.println(vehicles);
         }
-
-        System.out.println();
-    }
-    
-
-    // Método para excluir todos os veículos cadastrados
-    public static void excludeVehicles() {
-        System.out.println("=".repeat(10) + " Excluir Veículos " + "=".repeat(10));
     }
 }

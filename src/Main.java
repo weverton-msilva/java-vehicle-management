77e2c145dab4284039e472def4f5248a50671a4e
Weverton <wevertonmartinsdigital@gmail.com>
2026-06-24 12:32:44 -0300

    update vehicle manager create and implement in the program
    Refactor menu display and option handling in Main.java.

--- src/Main.java
+++ src/Main.java
@@ -1,49 +1,46 @@
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.InputMismatchException;
+import java.util.ArrayList;
 
 public class Main {
-    // Método de execução principal do programa
+    // Método principal para execução do programa
     public static void main (String[] args) {
+        // Objeto da instância Scanner para entrada de dados
         Scanner scanner = new Scanner(System.in);
 
+        // ArrayList para guarda os veículos cadastrados
         ArrayList<Vehicles> vehicles = new ArrayList<>();
 
-        // Loop para execução da interface e menu do usuário
-        boolean generateInterface = true;
+        // Chamar método do menu e receber valor do mesmo
+        int menuOption = viewMenu(scanner);
+    }
 
-        while (generateInterface) {
-            System.out.println("=".repeat(10) + " GESTÃO DE FROTAS SIMPLES " + "=".repeat(10));
 
-            System.out.println("\n" + """
-            1- Cadastrar Novos Veículos
-            2- Listar Todos Veículos
-            3- Excluir Veículo
-            4- Sair do Sistema""");;
+    // Método para exibição da interface do menu de opções
+    public static int viewMenu(Scanner scanner) {
+        while (true) {
+            System.out.println("=".repeat(10) + " VEHICLE MANAGEMENT " + "=".repeat(10));
 
-            int selectedOption;
+            System.out.println("""
+            Available Options:
+            1- Register New Vehicle
+            2- List All Vehicles
+            3- Edit Registered Vehicle
+            4- Delete Records
+            5- Exit the System""");
+            System.out.print("Option: ");
+            
+            // Receber a opção selecionada pelo usuário
+            String userOption = scanner.nextLine();
+            int menuOption;
 
+            // Validar a opção escolhida para prosseguir
             try {
-                System.out.print("Opção Selecionada: ");
-                selectedOption = scanner.nextInt();
+                menuOption = Integer.parseInt(userOption);
+                return menuOption;
                 
-            } catch (InputMismatchException e) {
-                System.out.println("\nOpção informada não está disponível entre as opções do menu.");
-                scanner.nextLine(); // Limpar buffer
+            } catch (NumberFormatException e) {
+                System.out.print("Only the use of numbers is permitted.\n");
                 continue;
-            }
-
-            switch (selectedOption) {
-                case 1: { // Redirecionar para o método de cadastro de novos veículos
-                    Vehicles vehicle = VehicleService.vehicleRegistration(scanner); 
-                    vehicles.add(vehicle);
-                    break;
-                }      
-
-                case 2: { // Redirecionar para o método de listagem de todos veículos
-                    VehicleService.listVehicles(vehicles);
-                    break;
-                }
             }
         }
     }
